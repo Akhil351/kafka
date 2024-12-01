fc65faf5dabc400911886438d77b3ea2773e6772
Akhil351 <akhil.vathaluru@gmail.com>
2024-12-02 00:35:37 +0530

    first commit

---
+++ base-domains/src/test/java/org/Akhil/base_domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Akhil.base_domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
